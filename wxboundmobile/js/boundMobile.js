84d56d1f95403412e0aca9e5151d301c23965231
MissZwei <1041090303@qq.com>
2017-11-24 11:43:41 +0800

    微信绑定手机号

--- wxboundmobile/js/boundMobile.js
+++ wxboundmobile/js/boundMobile.js
@@ -13,14 +13,17 @@ $(function() {
 	var timeNum = 60;
 	function dtime(){
 		timeNum--;
-		getcode.html(timeNum + 's');
+		$(".send").show();
+		getcode.hide();
+		
+		$(".send").val(timeNum + '秒可重新获取');
 		if(!timeNum) {
 			clearTimeout(t);
 			timeNum = 60;
 			setTimeout(function(){
-				getcode.html('获取验证码');
+				$(".send").hide();
+				getcode.show();
 			}, 1000);
-			
 		} else {
 			t = setTimeout(dtime, 1000);
 		}
@@ -35,48 +38,55 @@ $(function() {
 			},2000);
 		}else{
 			dtime();
+//			$.ajax({
+//				type:"post",
+//				data:{mobile:tel},
+//				url:"",
+//				success:function(data){
+//					var data = eval(data);
+//					if(data.flag == '1'){
+//						tip.html(data.msg).fadeIn();
+//						setTimeout(function(){
+//							tip.fadeOut();
+//						},2000);
+//					}else{
+//						tip.html(data.msg).fadeIn();
+//						setTimeout(function(){
+//							tip.fadeOut();
+//						},2000);
+//					}
+//				},
+//				error:function(err){
+//					console.log(err);
+//				}
+//			});
+		}
+	});
+	$(".confirm").on('click', function() {
+		var tel =  Number(mobile.val());
+		var code = codenum.val();
+		if(code==''){
+			tip.html('请输入验证码').fadeIn();
+			setTimeout(function(){
+				tip.fadeOut();
+			},2000);
+		}else if(tel!=''&&code!=''){
 			$.ajax({
 				type:"post",
-				data:{mobile:tel},
+				data:{mobile:tel,code:code},
 				url:"",
 				success:function(data){
 					var data = eval(data);
 					if(data.flag == '1'){
-						tip.html(data.msg).fadeIn();
-						setTimeout(function(){
-							tip.fadeOut();
-						},2000);
+						window.location.href="";
 					}else{
 						tip.html(data.msg).fadeIn();
 						setTimeout(function(){
 							tip.fadeOut();
 						},2000);
 					}
-				},
-				error:function(err){
-					console.log(err);
 				}
 			});
 		}
-	});
-	$(".confirm").on('click', function() {
-		var tel =  Number(mobile.val());
-		var code = codenum.val();
-		$.ajax({
-			type:"post",
-			data:{mobile:tel,code:code},
-			url:"",
-			success:function(data){
-				var data = eval(data);
-				if(data.flag == '1'){
-					window.location.href="";
-				}else{
-					tip.html(data.msg).fadeIn();
-					setTimeout(function(){
-						tip.fadeOut();
-					},2000);
-				}
-			}
-		});
 	});
 });
